22c9f3492963540d5f559d433c4c1cc4146b9139
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-05 23:39:56 +0200

    Add 8 Kyu: Even or Odd (ruby)

---
+++ 8_kyu/even_or_odd/even_or_odd.rb
@@ -0,0 +1,16 @@
+# URL:
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+#
+# Title:
+# Even or Odd
+#
+
+# v2
+def even_or_odd(number)
+  number.even? ? 'Even' : 'Odd'
+end
+
+# # v1
+# def even_or_odd(number)
+#   number.even? ? 'Even' : 'Odd'
+# end
