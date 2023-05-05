5627b0674fbc68a007122d8954d882b3550d9151
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-06 00:32:55 +0200

    Add 7 Kyu: Vowel Count (ruby)

---
+++ 7_kyu/vowel_count/vowel_count.rb
@@ -0,0 +1,16 @@
+# URL:
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
+#
+# Title:
+# Vowel Count
+#
+
+# v2
+def get_count(input_str)
+  input_str.count('aeiou')
+end
+
+# # v1
+# def get_count(input_str)
+#   input_str.scan(/[aeiou]/).count
+# end
