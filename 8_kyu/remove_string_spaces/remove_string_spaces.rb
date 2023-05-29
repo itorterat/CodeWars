2d6e4635b6e712c34c5399d8f4fce6d35f32cbef
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 01:32:14 +0200

    Add 8 Kyu: Remove String Spaces (ruby)

---
+++ 8_kyu/remove_string_spaces/remove_string_spaces.rb
@@ -0,0 +1,21 @@
+# URL:
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/ruby
+#
+# Title:
+# Remove String Spaces
+#
+
+# v3
+def no_space(x)
+  x.delete(' ')
+end
+
+# # v2
+# def no_space(x)
+#   x.gsub(' ', '')
+# end
+
+# # v1
+# def no_space(x)
+#   x.tr(' ', '')
+# end
