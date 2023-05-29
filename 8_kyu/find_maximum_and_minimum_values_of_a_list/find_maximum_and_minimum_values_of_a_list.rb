859b1927ed2f58cc38d4c63581a4ac6c8f46e06c
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-29 12:48:09 +0200

    Add 8 Kyu: Find Maximum and Minimum Values of a List (ruby)

---
+++ 8_kyu/find_maximum_and_minimum_values_of_a_list/find_maximum_and_minimum_values_of_a_list.rb
@@ -0,0 +1,15 @@
+# URL:
+# https://www.codewars.com/kata/577a98a6ae28071780000989/train/ruby
+#
+# Title:
+# Find Maximum and Minimum Values of a List
+#
+
+# v1
+def min(list)
+  list.min
+end
+
+def max(list)
+  list.max
+end
