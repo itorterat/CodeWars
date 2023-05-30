2b518b34235c399e9f8c07a27ce1cf5e336fc8ef
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 21:12:04 +0200

    Add 8 Kyu: Counting sheep... (ruby)

---
+++ 8_kyu/counting_sheep/counting_sheep.rb
@@ -0,0 +1,13 @@
+# URL:
+# https://www.codewars.com/kata/54edbc7200b811e956000556
+#
+# Title:
+# Counting sheep...
+#
+
+# def countSheeps on Codewars...
+
+# v1
+def count_sheeps(array)
+  array.count(true)
+end
