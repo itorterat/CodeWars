9becaf7c652d64daba751561204617b98e8b1882
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 23:45:52 +0200

    Add 8 Kyu: Remove exclamation marks (ruby)

---
+++ 8_kyu/remove_exclamation_marks/remove_exclamation_marks.rb
@@ -0,0 +1,16 @@
+# URL:
+# https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+#
+# Title:
+# Remove exclamation marks
+#
+
+# v2
+def remove_exclamation_marks(s)
+  s.delete('!')
+end
+
+# # v1
+# def remove_exclamation_marks(s)
+#   s.tr('!', '')
+# end
