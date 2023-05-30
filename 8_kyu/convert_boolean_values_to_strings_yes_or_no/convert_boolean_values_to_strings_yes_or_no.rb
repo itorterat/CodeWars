07938d79c9ce8bd321b0ba4ddc7cd07bd067b6e6
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 19:39:11 +0200

    Add 8 Kyu: Convert boolean values to strings 'Yes' or 'No'. (ruby)

---
+++ 8_kyu/convert_boolean_values_to_strings_yes_or_no/convert_boolean_values_to_strings_yes_or_no.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/53369039d7ab3ac506000467
+#
+# Title:
+# Convert boolean values to strings 'Yes' or 'No'.
+#
+
+# v1
+def bool_to_word(bool)
+  bool ? 'Yes' : 'No'
+end
