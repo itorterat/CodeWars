84bba1620c45f078d99640bb3604800a18bc2c9e
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 22:18:21 +0200

    Add 8 Kyu: Return Negative (ruby)

---
+++ 8_kyu/return_negative/return_negative.rb
@@ -0,0 +1,16 @@
+# URL:
+# https://www.codewars.com/kata/55685cd7ad70877c23000102
+#
+# Title:
+# Return Negative
+#
+
+# v2
+def make_negative(num)
+  -num.abs
+end
+
+# # v1
+# def make_negative(num)
+#   num.positive? ? -num : num
+# end
