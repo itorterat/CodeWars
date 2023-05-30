475100effb21e98f5ea373e2acee47007b8a36e8
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 22:23:25 +0200

    Add 7 Kyu: Sum of two lowest positive integers (ruby)

---
+++ 7_kyu/sum_of_two_lowest_positive_integers/sum_of_two_lowest_positive_integers.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/558fc85d8fd1938afb000014
+#
+# Title:
+# Sum of two lowest positive integers
+#
+
+# v1
+def sum_two_smallest_numbers(numbers)
+  numbers.min(2).sum
+end
