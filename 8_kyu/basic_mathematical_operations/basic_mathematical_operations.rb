b15a841f5607a584e6f1a88367a68e39eebb4fe7
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-30 22:48:01 +0200

    Add 8 Kyu: Basic Mathematical Operations (ruby)

---
+++ 8_kyu/basic_mathematical_operations/basic_mathematical_operations.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+#
+# Title:
+# Basic Mathematical Operations
+#
+
+# v1
+def basic_op(operator, value1, value2)
+  value1.send(operator, value2)
+end
