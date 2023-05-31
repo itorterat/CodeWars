3852d273328ef8a63d08d784bc17b4f9f9468f22
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-31 14:00:55 +0200

    Add 8 Kyu: Will you make it? (ruby)

---
+++ 8_kyu/will_you_make_it/will_you_make_it.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/5861d28f124b35723e00005e
+#
+# Title:
+# Will you make it?
+#
+
+# v1
+def zero_fuel(distance, mpg, fuel_left)
+  distance <= mpg * fuel_left
+end
