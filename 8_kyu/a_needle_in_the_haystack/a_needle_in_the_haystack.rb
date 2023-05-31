7f43760eb5fa23cc4776af2014d21a23da24e10c
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-05-31 11:53:41 +0200

    Add 8 Kyu: A Needle in the Haystack (ruby)

---
+++ 8_kyu/a_needle_in_the_haystack/a_needle_in_the_haystack.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+#
+# Title:
+# A Needle in the Haystack
+#
+
+# v1
+def find_needle(haystack)
+  "found the needle at position #{haystack.index('needle')}"
+end
