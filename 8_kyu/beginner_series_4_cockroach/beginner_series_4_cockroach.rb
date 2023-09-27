f435ef8c211fb42b2dfe32aa0684bbe15a96b25e
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-27 19:16:42 +0200

    Add 8 Kyu: Beginner Series #4 Cockroach (ruby)

---
+++ 8_kyu/beginner_series_4_cockroach/beginner_series_4_cockroach.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+#
+# Title:
+# Beginner Series #4 Cockroach
+#
+
+# v1
+def cockroach_speed(speed_kmh)
+  (speed_kmh * 100_000 / 3600).floor
+end
