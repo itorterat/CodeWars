063a73e2bc24ea1c058cc25c2d957a68d45e901e
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-27 20:27:33 +0200

    Add 8 Kyu: SQL Basics: Simple MIN / MAX (SQL)

---
+++ 8_kyu/sql_basics_simple_min_max/sql_basics_simple_min_max.sql
@@ -0,0 +1,10 @@
+-- URL:
+-- https://www.codewars.com/kata/581113dce10b531b1d0000bd
+--
+-- Title:
+-- SQL Basics: Simple MIN / MAX
+--
+
+-- v1
+SELECT MIN(age) AS age_min, MAX(age) AS age_max
+FROM people;
