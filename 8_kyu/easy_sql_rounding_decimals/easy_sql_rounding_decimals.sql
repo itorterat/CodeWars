39c9e51dcd9052f8750650dfe00e78b1e741eef6
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-27 20:23:36 +0200

    Add 8 Kyu: Easy SQL: Rounding Decimals (SQL)

---
+++ 8_kyu/easy_sql_rounding_decimals/easy_sql_rounding_decimals.sql
@@ -0,0 +1,10 @@
+-- URL:
+-- https://www.codewars.com/kata/594a6133704e4daf5d00003d
+--
+-- Title:
+-- Easy SQL: Rounding Decimals
+--
+
+-- v1
+SELECT FLOOR(number1) AS number1, CEILING(number2) AS number2
+FROM decimals;
