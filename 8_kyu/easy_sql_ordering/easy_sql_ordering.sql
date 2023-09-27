c7b1a28f959c41b663ab2aa0905b7d8cdac35aab
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-27 19:41:51 +0200

    Add 8 Kyu: Easy SQL - Ordering (SQL)

---
+++ 8_kyu/easy_sql_ordering/easy_sql_ordering.sql
@@ -0,0 +1,11 @@
+-- URL:
+-- https://www.codewars.com/kata/593ed37c93350098d600001d
+--
+-- Title:
+-- Easy SQL - Ordering
+--
+
+-- v1
+SELECT id, ceo, motto, employees
+FROM companies
+ORDER BY employees DESC;
