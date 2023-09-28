b6dad2de0de9a99e4a1ee0285a8ddd061e053992
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-28 11:55:09 +0200

    Add 8 Kyu: Register for the Party (SQL for Beginners #3) (SQL)

---
+++ 8_kyu/register_for_the_party_sql_for_beginners_3/register_for_the_party_sql_for_beginners_3.sql
@@ -0,0 +1,13 @@
+-- URL:
+-- https://www.codewars.com/kata/590cc86f7557c0494000007e
+--
+-- Title:
+-- Register for the Party (SQL for Beginners #3)
+--
+
+-- v1
+INSERT
+  INTO participants (name, age, attending)
+  VALUES ('Joe', 27, true);
+
+SELECT * FROM participants;
