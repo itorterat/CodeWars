d789f78b7f321ebf174b3cc80a9b50d535d0ef9f
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-28 12:12:15 +0200

    Add 8 Kyu: Easy SQL: Convert to Hexadecimal (SQL)

---
+++ 8_kyu/easy_sql_convert_to_hexadecimal/easy_sql_convert_to_hexadecimal.sql
@@ -0,0 +1,10 @@
+-- URL:
+-- https://www.codewars.com/kata/594a50bafd3b7031c1000013
+--
+-- Title:
+-- Easy SQL: Convert to Hexadecimal
+--
+
+-- v1
+SELECT TO_HEX(arms) AS arms, TO_HEX(legs) AS legs
+FROM monsters;
