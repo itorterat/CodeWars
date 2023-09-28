55aa3317ed0ca9038a073131989f5438ca7cc5ac
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-28 12:18:39 +0200

    Add 8 Kyu: Grasshopper - Terminal game move function (SQL)

---
+++ 8_kyu/grasshopper_terminal_game_move_function/grasshopper_terminal_game_move_function.sql
@@ -0,0 +1,10 @@
+-- URL:
+-- https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+--
+-- Title:
+-- Grasshopper - Terminal game move function
+--
+
+-- v1
+SELECT (position + roll*2) AS res
+FROM moves;
