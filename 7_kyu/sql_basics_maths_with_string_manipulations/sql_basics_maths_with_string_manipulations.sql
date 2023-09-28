d21eff7d79c4077672132fc88c70cf140913477a
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-28 17:26:10 +0200

    Add 7 Kyu: SQL Basics: Maths with String Manipulations (SQL)

---
+++ 7_kyu/sql_basics_maths_with_string_manipulations/sql_basics_maths_with_string_manipulations.sql
@@ -0,0 +1,10 @@
+-- URL:
+-- https://www.codewars.com/kata/594901ba44645fd7bd00005f
+--
+-- Title:
+-- SQL Basics: Maths with String Manipulations
+--
+
+-- v1
+SELECT (BIT_LENGTH(name) + LENGTH(race)) AS calculation
+FROM demographics;
