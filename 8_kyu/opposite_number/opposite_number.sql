6bd21351c03ea2b5d0d87e7d6e3ae02c11049dc4
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-28 12:25:28 +0200

    Add 8 Kyu: Opposite number (SQL)

---
+++ 8_kyu/opposite_number/opposite_number.sql
@@ -0,0 +1,14 @@
+-- URL:
+-- https://codewars.com/kata/56dec885c54a926dcd001095
+--
+-- Title:
+-- Opposite number
+--
+
+-- v2
+SELECT -number AS res
+FROM opposite;
+
+-- v1
+SELECT (number*-1) AS res
+FROM opposite;
