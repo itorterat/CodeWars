028921df27af2e740c3b5559cfaaeea7e7303193
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-09-29 15:38:29 +0200

    Add 8 Kyu: Grasshopper - Check for factor (ruby)

---
+++ 8_kyu/grasshopper_check_for_factor/grasshopper_check_for_factor.rb
@@ -0,0 +1,11 @@
+# URL:
+# https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+#
+# Title:
+# Grasshopper - Check for factor
+#
+
+# v1
+def check_for_factor(base, factor)
+  (base % factor).zero?
+end
